4ee5f27c038d79d587e6dc1034218054e7e8521e
Romanchik <chikroman@mail.ru>
2022-05-30 21:00:48 +0300

    ДЗ кто хочет стать миллионером часть 1

---
+++ Les_5_3_1/millionaire/millionaire/Controllers/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  millionaire
+//
+//  Created by Роман Чикишев on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var StartButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "startGameSegue":
+            guard segue.destination is GameController else {
+                return
+            }
+            
+        default:
+            break
+        }
+    }
+
+}
+
