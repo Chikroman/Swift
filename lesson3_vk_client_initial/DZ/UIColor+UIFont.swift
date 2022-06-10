3daed6b84ec51ddd2ef3ff6a0f57c5ec80226f86
Romanchik <chikroman@mail.ru>
2022-06-10 17:00:11 +0300

    ДЗ 3

---
+++ lesson3_vk_client_initial/DZ/UIColor+UIFont.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+UIFont.swift
+//  homeWork_1
+//
+//  Created by Роман Чикишев on 10.06.2022.
+//  Copyright © 2022 Марат Нургалиев. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColorBlack = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)
+    static let backgroundColor = UIColor(red: 24/255, green: 139/255, blue: 243/255, alpha: 1)
+    static let strokeColor = UIColor(red: 238/255, green: 243/255, blue: 251/255, alpha: 1)
+    static let fillColor = UIColor(red: 92/255, green: 175/255, blue: 248/255, alpha: 1)
+}
